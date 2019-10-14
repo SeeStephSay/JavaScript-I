d2d1ebf40580c72094350a8418b112092ca466ba
Stephanie Butenhof <1divinedevotion@gmail.com>
2019-10-14 15:07:34 -0500

    Converted functions to single-line arrow functions.

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,19 +5,13 @@ console.log("Function was invoked!");
 };
 myFunction();
 
-let anotherFunction = (param) => {
-    return param;
-};
+let anotherFunction = param => param;
 anotherFunction("Example");
 
-let add = (param1, param2) => {
-    return param1 + param2;
-};
+let add = (param1, param2) => param1 + param2;
 add(1,2);
 
-let subtract = (param1, param2) => {
-    return param1 - param2;
-};
+let subtract = (param1, param2) => param1 - param2;
 subtract(1,2);
 
 
